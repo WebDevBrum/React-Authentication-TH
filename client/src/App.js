e9bd182a9372187050a657f7d01c64012edeeb34
Scott Davies <scottdavies1983@gmail.com>
2020-09-11 08:15:19 +0100

    progress made

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import Header from './components/Header';
+import Public from './components/Public';
+import NotFound from './components/NotFound';
+import UserSignUp from './components/UserSignUp';
+import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
+import Authenticated from './components/Authenticated';
+
+// New import
+import withContext from './Context';
+import PrivateRoute from './PrivateRoute';
+
+// Connect the Header component to context
+const HeaderWithContext = withContext(Header);
+
+const AuthWithContext = withContext(Authenticated);
+
+// Connect UserSignUp to context
+const UserSignUpWithContext = withContext(UserSignUp);
+// Connect UserSignIn to context
+const UserSignInWithContext = withContext(UserSignIn);
+
+export default () => (
+  <Router>
+    <div>
+      <HeaderWithContext />
+
+      <Switch>
+        <Route exact path="/" component={Public} />
+        <PrivateRoute path="/authenticated" component={AuthWithContext} />
+        <Route path="/signin" component={UserSignInWithContext} />
+        <Route path="/signup" component={UserSignUpWithContext} />
+        <Route path="/signout" component={UserSignOut} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
---
+++ client/src/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { Consumer } from './Context';
+
+// redirects to sign in if authenticated user does not exist in state
+
+export default ({ component: Component, ...rest }) => {
+  return (
+    <Consumer>
+      { context => (
+        <Route
+          {...rest}
+          render={props => context.authenticatedUser ? (
+              <Component {...props} />
+            ) : (
+              <Redirect to='/signin' />
+            )
+          }
+        />
+      )}
+    </Consumer>
+  );
+};
